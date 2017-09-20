3a96676b530861794b4e3f46887692f9513f7262
AlexaInga2 <alexainga1@gmail.com>
2017-09-20 00:20:50 +0000

    adding parameter description in comment section

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,6 +2,7 @@
 #include <ctype.h>
 /**
  * _islower -  checks for lowercase character
+ * @c: parameter to loop through to see if char is lowercase
  *
  * Return: 1 if c is lowecase, otherwise 0
  */
