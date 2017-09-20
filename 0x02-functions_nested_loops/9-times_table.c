70d9aa458ac58b388410204064ec19f2089c94c1
AlexaInga2 <alexainga1@gmail.com>
2017-09-20 05:18:37 +0000

    adding header filer

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * times_table - prints the 9 times table, starting with 0
+ *
+ *
+ */
+void times_table(void)
+{
+	int i, j, n;
+
+        for (i = 0; i < 10; _putchar('\n'), i++)
+        {
+                _putchar('0');
+                _putchar(',');
+                for (j = 1; j < 10; j++)
+                {
+                        n = i * j;
+                        _putchar(' ');
+                        _putchar(n / 10 ? n / 10 + '0' : ' ');
+                        _putchar(n % 10 + '0');
+                        _putchar(',');
+                }
+        }
+}
