6f714a9472f93e867aa080b878332cb116027993
AlexaInga2 <alexainga1@gmail.com>
2017-09-22 00:07:44 +0000

    functions to multiply digits, print numbers, print alphabet

---
+++ 0x03-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * mul - function that multiplies two integers.
+ * @a: first paraemeter to multiply
+ * @b: second parameter to multiply
+ *
+ * Return: the multiplied number of a * b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
---
+++ 0x03-more_functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+Topics cover: nested loops and how to use them, functions and how to use them
+Prototypes, scope of variables, gcc flags, and header files
+Functions that:
