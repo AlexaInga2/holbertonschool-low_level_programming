8131435b58cbe6f21eb5b95e72566912a79d834f
AlexaInga2 <alexainga1@gmail.com>
2017-10-06 02:27:16 +0000

    returns the length of a string

---
+++ 0x07-recursion/2-strlen_recursion.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * _strlen_recursion - calculates the length of the string s
+ * excluding the terminating null byte ('\0')
+ * @s: the string
+ *
+ * Return: length of s / number of bytes in the string s
+ */
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+
+	return(_strlen_recursion(s + 1) + 1);
+}
