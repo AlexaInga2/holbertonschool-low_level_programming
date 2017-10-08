835c1e0ef83fe85e312c2b2e3d4c56484c3a9cb6
AlexaInga2 <alexainga1@gmail.com>
2017-10-08 22:30:04 +0000

    function that convert a string to an integer & adding prototype to header file

---
+++ 0x04-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _atoi - function that convert a string to an integer
+ *
+ * @s: the string to be converted
+ *
+ * Return: converted string
+ */
+int _atoi(char *s)
+{
+	int i, j, m, n;
+
+	i = n = 0;
+	m = 1;
+
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			m *= -1;
+		i++;
+	}
+	j = i;
+	while ((s[j] >= '0') && (s[j] <= '9'))
+	{
+		n = n * 10 + m * (s[j] - '0');
+		j++;
+	}
+	return (n);
+}
