e6cbe3a4da8836500ded14562c97750207abf046
AlexaInga2 <alexainga1@gmail.com>
2017-10-13 06:56:20 +0000

    fixing syntax

--- 0x0B-more_malloc_free/holberton.h
+++ 0x0B-more_malloc_free/holberton.h
@@ -1,7 +1,9 @@
 #ifndef _HOLBERTON_H_
 #define _HOLBERTON_H_
 
-int _putchar(char c);
+void _putchar(char c);
+int _strlen(char *s);
+
 void *malloc_checked(unsigned int b);
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 void *_calloc(unsigned int nmemb, unsigned int size);
