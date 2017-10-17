fc1546de92f6a24a2f6344a746cac0ef4a7ff959
AlexaInga2 <alexainga1@gmail.com>
2017-10-17 05:45:30 +0000

    program that prints name of source file

---
+++ 0x0C-preprocessor/2-main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  prints name of source file, followed by a new line.
+ *
+ * Return: 0 always
+ */
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
