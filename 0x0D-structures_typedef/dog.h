fcabaa411f01d8668f5ce6ed0a291dd374590b2e
AlexaInga2 <alexainga1@gmail.com>
2017-10-17 07:02:30 +0000

    adding putchar function

---
+++ 0x0D-structures_typedef/dog.h
@@ -0,0 +1,22 @@
+#ifndef _DOG_H_
+#define _DOG_H_
+/**
+ * struct dog - the puppies!! & all their cuteness
+ * @name: name of dog (puppy)
+ * @age: age of dog (puppy)
+ * @owner: name of owner
+ */
+typedef struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+} dog_t;
+/* or  typedef struct dog_t - new type  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+_int _putchar(char c)l
+#endif /* _DOG_H_ */
