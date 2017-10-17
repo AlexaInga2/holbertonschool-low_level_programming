f83ba7f7e6f2d04b11876a0ab85bece626ab9a38
AlexaInga2 <alexainga1@gmail.com>
2017-10-17 07:34:47 +0000

    pushing header file

--- 0x0D-structures_typedef/dog.h
+++ 0x0D-structures_typedef/dog.h
@@ -12,11 +12,8 @@ typedef struct dog
 	float age;
 	char *owner;
 } dog_t;
-/* or  typedef struct dog_t - new type  */
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-_int _putchar(char c)l
 #endif /* _DOG_H_ */
