3abd27b10ce2c078a8f9b6bdfdc7e7242385a79b
AlexaInga2 <alexainga1@gmail.com>
2017-11-01 07:06:19 +0000

    removing semicolon from prototype

--- 0x12-more_singly_linked_lists/1-listint_len.c
+++ 0x12-more_singly_linked_lists/1-listint_len.c
@@ -7,7 +7,7 @@
  * @h: pointer to the first node of linked list
  * Return: the number of nodes
  */
-size_t listint_len(const listint_t *h);
+size_t listint_len(const listint_t *h)
 {
 	size_t node_count = 0;
 
