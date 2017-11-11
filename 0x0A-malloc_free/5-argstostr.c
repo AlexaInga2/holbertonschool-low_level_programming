78063e395c327227cd1fcf5680466950bb14fc88
AlexaInga2 <alexainga1@gmail.com>
2017-11-11 08:37:58 +0000

    function that concentrates all arguments

---
+++ 0x0A-malloc_free/5-argstostr.c
@@ -0,0 +1,38 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * argstostr - a function that concentrates all program's arguments
+ * @ac: number of arguments w/i string
+ * @av: array of arguments, double pointer array
+ *
+ * Return: pointer/& to a new string, or NULL if ac or av = 0 | fails
+ */
+char *argstostr(int ac, char **av)
+{
+	int index, count, sum;
+	char *nptr = NULL;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	for (index = 0; index < ac; index++)
+	{
+		for (count = 0; av[index][count]; count++)
+			;
+		sum = count + 1;
+	}
+	nptr = malloc(sum * sizeof(char) + 1);
+
+	for (index = 0; index < ac; index++)
+	{
+		for (count = 0; av[index][count]; count++)
+		{
+			nptr[sum] = av[index][count];
+			sum++;
+		}
+		nptr[sum] = '\n';
+		sum++;
+	}
+	nptr[sum] = '\0';
+	return (nptr);
+}
